97cc56e78fe8e13717432976b6f788696067bbda
DenisAstashkin <ast.den27@gmail.com>
2024-11-17 23:48:01 +0400

    Add models

---
+++ CommunalController/Models/PaymentInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace CommunalController.Models
+{
+    public class PaymentInfo
+    {
+        public string TypeOfPayment { get; set; }
+        public int Size { get; set; }
+        public double Rate {  get; set; }
+        public double Accrued { get; set; }
+    }
+}
---
+++ CommunalController/Models/PersonInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace CommunalController.Models
+{
+    public class PersonInfo
+    {
+        public string IdStorage { get; set; }
+        public string FullName { get; set; }
+        public string DateCreate { get; set; }
+        public string Address { get; set; }
+        public string PayDate { get; set; }
+    }
+}
